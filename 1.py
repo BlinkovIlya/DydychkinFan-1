44396e5695fa1380f765b3c97be9c22a5d765853
Ilya Blincov <ioblinkov@edu.hse.ru>
2022-10-14 14:20:31 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+a = str(input("Введите имя: "))
+b = str(input("Введите фамилию: "))
+c = int(input("Введите год: "))
+print(a+'_'+b+'_'+str(c))
+a,b = b,a
+c = c+60
+print(a+'_'+b+'_'+str(c))
+
+        
---
+++ 21.py
@@ -0,0 +1,6 @@
+a = int(input("Введите первый катет: "))
+b = int(input("Введите второй катет: "))
+import math
+c = math.sqrt(a*a+b*b)
+print("Площадь равна: ", (a*b)/2)
+print("Периметр равен: ", a+b+c)
